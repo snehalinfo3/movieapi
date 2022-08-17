9b56b477406c769781686a18e456e831276f0c79
snl <snl.com>
2022-08-17 16:39:37 +0530

    adding files

---
+++ moviesApi/Data/moviesApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using moviesApi.Models;
+
+namespace moviesApi.Data
+{
+    public class moviesApiContext : DbContext
+    {
+        public moviesApiContext (DbContextOptions<moviesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<moviesApi.Models.Movie> Movie { get; set; }
+    }
+}
